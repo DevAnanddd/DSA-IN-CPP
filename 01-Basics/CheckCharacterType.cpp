eb414f9089ef8cf381d46ee1db998277b5903c5d
Dev Anand <devanand22052007@gmail.com>
2026-08-06 18:31:09 +0530

    Program to identify character type

---
+++ 01-Basics/CheckCharacterType.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char ch;
+cout<<"Enter the character"<<endl;
+cin>>ch;
+
+if(ch>= 'a' && ch<= 'z'){
+    cout<<"lower case"<<endl;
+}
+else if(ch>= 'A' && ch<= 'Z'){
+    cout<<"Upper case"<<endl;
+}
+else if(ch>= '0' && ch<= '9'){
+    cout<<"Numeric"<<endl;
+}
+else{
+    cout<<"Symbol"<<endl;
+}
+
+return 0;
+}
